3d790937f5db4a982477ecf4c2241f4b4fbdd360
Alex <office@vincenz.at>
2022-06-29 15:16:45 +0200

    test2

--- test2/src/Meins.jsx
+++ test2/src/Meins.jsx
@@ -1,7 +1,17 @@
 import './Meins.css';
+import {useState} from 'react';
 
 function Meins( props ) {
     console.log( props );
+
+   /* const [counter,setCounter ] = useState(0);
+    const clickHandler = (e,i)=>{
+        e.preventDefault();
+        //counter++;
+        setCounter( counter+i ); // async
+        console.log( counter );
+      }*/
+
     let h1tag = '';
     if ( props.title ) {
         // h1tag = '<h1>'+props.title+'</h1>';
@@ -32,6 +42,8 @@ function Meins( props ) {
             {/*  mach H1 nur wenn props.title nicht leer ist */ }
             {props.title?(<h1>{props.title}</h1>):(<h2>no title</h2>)}
             <p>{props.children?props.children:'Hello World!'}</p>
+            mein Counter: {props.counter}
+            <a href="#" onClick={e=>{e.preventDefault(); props.reset(); }}>reset</a>
         </>
     );
 
