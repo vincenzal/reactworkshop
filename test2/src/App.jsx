33994494bf69d4759ae4871cdde053631342965a
Alex <office@vincenz.at>
2022-06-29 15:16:38 +0200

    test2 done

--- test2/src/App.jsx
+++ test2/src/App.jsx
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Meins from './Meins';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
   //let counter = 0;
 
   const [counter,setCounter ] = useState(0);
+
+ useEffect( ()=>{
+  fetch('http://wifi.1av.at/random.php?min=1&max=10')
+  .then( response => response.text() )
+  .then( data => {
+    console.log( data );
+    setCounter( Number(data) );
+  })
+ },[]);
+
+
+  const resetCounter = ()=>{
+    setCounter(0);
+  }
 
   const clickHandler = (e,i)=>{
     e.preventDefault();
@@ -21,6 +35,8 @@ function App() {
       <header className="App-header">
         <Meins
           title={<span>hello</span>}
+          counter={counter}
+          reset={resetCounter}
         >
           <span>Text für Komponente</span>
         </Meins>
@@ -28,7 +44,8 @@ function App() {
         <button id="btn" onClick={(e)=>{clickHandler(e,1)}}>+1</button>
         <button id="btn2" onClick={ event=>{ clickHandler(event,2) } }>+2</button>
         <Meins
-        
+          counter={counter}
+          reset={resetCounter}
         ></Meins>
       </header>
     </div>
