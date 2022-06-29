0ae2b5583c7cd0572ee6d76fc297365e1ed4bb0a
Alex <office@vincenz.at>
2022-06-29 15:33:26 +0200

    todo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+//https://www.w3schools.com/howto/howto_js_todolist.asp
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoliste">
+      TODOS...
     </div>
   );
 }
